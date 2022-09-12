0625ffba9fd5eb278037607d91940ebb0268b309
Alessandro <ale.diliberti@gmail.com>
2022-09-12 16:43:10 +0200

    Working with bars, pie, table

--- Codice/gui.py
+++ Codice/gui.py
@@ -7,12 +7,15 @@ root = Tk()
 root.geometry('300x350')
 root.eval('tk::PlaceWindow . center')
 
-# Create a listbox
+# Creo a listbox
 listbox = Listbox(root, width=40, height=20, selectmode=MULTIPLE)
 
 df = pd.read_csv('country_density.csv', encoding='UTF-8')
+#Riempio listbox con i valori del dataframe
 for i  in range(len(df)):
     listbox.insert(i,df.values[i][1])
+#funzione che una volta premuto il bottone aggiunge i valori ad un sub-dataframe
+#e costruisce i 3 grafici
 def selected_item():
     indexes=[]
     for i in listbox.curselection():
@@ -30,23 +33,26 @@ def selected_item():
     # costruiamo il grafico a torta
     pie = px.pie(dataframe, values='Density_(P/Km²)', names='Country', title="Densità di popolazione")
 
+    #costruiamo una tabella
     table = go.Figure(data=[go.Table(
-        header=dict(values=list(df.columns),
+        header=dict(values=list(dataframe.columns[1:]),
                     fill_color='paleturquoise',
                     align='left'),
-        cells=dict(values=[df.Country, df.Density],
+        cells=dict(values=[dataframe.iloc[:,1:2],dataframe.iloc[:,2:]],
                    fill_color='lavender',
-                   align='left'))
+                   align='left')),
+
     ])
 
-    # plottiamo entrambi i grafici
+    #plottiamo tutti i grafici
+
     bar1.show()
     pie.show()
+    table.show()
 
 
 btn = Button(root, text='Print Selected', command=selected_item)
 
-# Placing the button and listbox
 btn.pack(side='bottom')
 listbox.pack()
 
